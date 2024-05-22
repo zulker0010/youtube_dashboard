9dcaac92a49ddcf018aced249260f00effc2b4e6
zulker0010 <zulkeryusuf@gmail.com>
2024-05-22 23:48:39 +0600

    built ranked_table ~ needs progress bar

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,13 @@
 import streamlit as st
+import numpy as np 
+import pandas as pd
+
+
+
+st.set_page_config(
+    page_title='YouTube Dashboard',
+    page_icon=None,
+    layout='wide'
+)
+
+
--- preprocessor_script.py
+++ preprocessor_script.py
@@ -1,9 +1,19 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
-import os
-import csv
+import openpyxl
 
 data_source = pd.read_excel(r"F:\Data Analytics\youtube_dashboard\Global_YouTube_Statistics.xlsx")
-df = pd.DataFrame(data_source)
 
-print(data_source)
+top_ranked_channels = data_source.iloc[:,:3]
+print(top_ranked_channels)
+
+col = st.columns((1,4.5,2.5), gap = 'small')
+
+
+with col[2]:
+    st.markdown('Top 10 YouTube Channels')
+    st.dataframe(
+    top_ranked_channels, 
+    hide_index = True
+    )
