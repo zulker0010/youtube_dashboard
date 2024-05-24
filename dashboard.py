a4037de27a756608fa00548df1c099295380ab9e
zulker0010 <zulkeryusuf@gmail.com>
2024-05-25 00:45:04 +0600

    wrote preprocessing script in notebook in Scripts~dropped and reshaped df

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,24 @@
-import streamlit as st
 import numpy as np 
 import pandas as pd
+import streamlit as st
+import openpyxl
 
 
+data_source = pd.read_excel(r"F:\Data Analytics\youtube_dashboard\Global_YouTube_Statistics.xlsx")
 
-st.set_page_config(
-    page_title='YouTube Dashboard',
-    page_icon=None,
-    layout='wide'
-)
+top_ranked_channels = data_source.iloc[:,:3]
+print(top_ranked_channels)
+
+st.title(':jetblack[YouTube Dashboard📈]')
+
+col = st.columns((1,5,4), gap = 'small')
+
+with col[2]:
+    st.markdown('Top 10 YouTube Channels')
+    
+    st.dataframe(
+        top_ranked_channels, 
+        hide_index = True,
+    )
 
 
--- preprocessor_script.py
+++ preprocessor_script.py
@@ -1,19 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import altair as alt
 import openpyxl
+import plotly_express as plx
 
 data_source = pd.read_excel(r"F:\Data Analytics\youtube_dashboard\Global_YouTube_Statistics.xlsx")
-
-top_ranked_channels = data_source.iloc[:,:3]
-print(top_ranked_channels)
-
-col = st.columns((1,4.5,2.5), gap = 'small')
-
-
-with col[2]:
-    st.markdown('Top 10 YouTube Channels')
-    st.dataframe(
-    top_ranked_channels, 
-    hide_index = True
-    )
